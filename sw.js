e8bb99c077b92db35b50aa7ecac2cefddb2e81a2
giovannycarvajald-arch <giovannycarvajald@gmail.com>
2026-09-08 22:35:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="presupuesto-v2-2";
-const ASSETS=["./","./index.html","./manifest.json","./icons/icon-180.png","./icons/icon-512.png"];
+const CACHE="presupuesto-v2-4";
+const ASSETS=["./","./index.html","./manifest.json","./sw.js","./apple-touch-icon.png","./icons/icon-180.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
