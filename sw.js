3d1e54bf99db3586c43aa73409a89f15cd7dab00
giovannycarvajald-arch <giovannycarvajald@gmail.com>
2026-09-08 22:03:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE="presupuesto-v1";
-const ASSETS=["./","./index.html","./manifest.json","./icons/icon-180.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="presupuesto-v2-1";const ASSETS=["./","./index.html","./manifest.json","./icons/icon-180.png","./icons/icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
